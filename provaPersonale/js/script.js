e3e291760b68e9b7ead4021bc9ea5234e0882734
AntonelloStefania <antonelloste94@gmail.com>
2023-05-31 21:47:01 +0200

    [fix] ordine cartelle

--- provaPersonale/js/script.js
+++ provaPersonale/js/script.js
@@ -37,8 +37,6 @@ const team=[
     }
 ]
 
-console.log(team)
-
 //recupero elemento DOM
 const main_container = document.getElementById('main');
 
@@ -46,8 +44,23 @@ const main_container = document.getElementById('main');
 for(let i=0; i<team.length;i++){
     let member=team[i];
     console.log(member)
+    let memberClass = `${i}`;
+    console.log(memberClass)
+    if(i===0){
+        memberClass += ' center z-ind-3 scale-12'
+    } else if(i===1){
+        memberClass += ' low-left  z-ind-2 scale-1'
+    } else if(i===2){
+        memberClass += ' mid-left  z-ind-1 scale-08 '
+    } else if(i===3){
+        memberClass += ' max-left scale-07'
+    } else if(i===4){
+        memberClass += ' low-right scale-08'
+    } else if(i===5){
+        memberClass += ' max-right z-ind-2 scale-1 '
+    }
 
-   let card=  `<div class="card width filter" >
+   let card=  `<div class=" card width filter ${memberClass}" >
                     <img class="card-img-top" src="./img/${member.image}" alt="Card image cap">
                     <div class="card-body">
                       <h5 class="card-title">${member.name}</h5>
@@ -64,11 +77,24 @@ for(let i=0; i<team.length;i++){
     let all_cards= document.getElementsByClassName('card')
     all_cards[active_card].classList.add('z-index-max')
     all_cards[active_card].classList.remove('filter')
+    // all_cards[active_card].classList.remove('scale-12')
+    //all_cards[active_card].classList.remove(`${memberClass}`)
+   
+   //prova movimento
+    all_cards[active_card].classList.add('center-center')
+
+    
+    
     const btn_next= document.getElementById('btn-next').
     addEventListener('click', function(){
-        
+
         all_cards[active_card].classList.add('filter')
         all_cards[active_card].classList.remove('z-index-max')
+        all_cards[active_card].classList.remove('scale-12')
+        
+        //prova movimento
+        all_cards[active_card].classList.remove('center-center');
+        all_cards[active_card].classList.add('low-left');
         active_card++
         
             for(let i=0; i<team.length; i++){
@@ -79,5 +105,12 @@ for(let i=0; i<team.length;i++){
             
             all_cards[active_card].classList.add('z-index-max');
             all_cards[active_card].classList.remove('filter')
+            all_cards[active_card].classList.add('scale-12')
+
+            //prova movimento
+             all_cards[active_card].classList.add('center-center')
+             all_cards[active_card].classList.remove('low-left');
+           
     })
+
 }
